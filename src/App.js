9ffe65f07737ef2ab5f9d8f77b34e31acd2a9ffe
SeongHyun Park <okpc0306@naver.com>
2021-04-09 00:08:08 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TestList from "./mbtiPage/testList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {};
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="test__list">
+        <h1>Hello World!</h1>
+        <TestList />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/mbtiPage/testList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function testList() {
+  return <h2>Hello World! testList!</h2>;
+}
+
+export default testList;
